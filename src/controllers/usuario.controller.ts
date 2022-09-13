1d01ade8b0ca4728cd5a1c01fff50c94219d9eba
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-09-13 18:20:04 -0300

    login usuario

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import connection from '../db/connection';
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const addUsuario = async (req: Request, res: Response) => {
 
@@ -17,5 +18,49 @@ export const addUsuario = async (req: Request, res: Response) => {
             })
         }
     })
+}
 
+export const loginUser = (req: Request, res: Response) => {
+    
+    const { nombre, password } = req.body;
+
+    connection.query('SELECT * FROM usuarios WHERE nombre = ' + connection.escape(nombre), (err, data) => {
+        if(err) {
+            console.log(err)
+        } else {
+            if(data.length == 0) {
+                // No existe el usuario en la base de datos
+                res.json({
+                    msg: 'No existe el usuario en la base de datos',                    
+                })
+            } else {
+                // Existe
+                const userPassword = data[0].password;
+                console.log(password)
+                // Comparamos el password
+                bcrypt.compare(password, userPassword).then((result) => {
+                    if(result) {
+                        // Login exitoso -- Generamos el token
+                        const token = jwt.sign({
+                            nombre: nombre,
+                        }, process.env.SECRET_KEY || 'pepito123')
+
+                        res.json({
+                            token
+                        })
+                    } else {
+                        // Password incorrecto
+                        res.json({
+                            msg: 'Password incorrecto',
+                        })
+                    }
+                })
+
+                
+            }           
+        }
+    })
+
+
+    
 }
